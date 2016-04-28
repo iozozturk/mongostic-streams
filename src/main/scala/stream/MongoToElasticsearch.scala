939c48090ecfbe94cd56bb841305b57e2e9219b2
İsmet Özöztürk <iozozturk@gmail.com>
2016-04-28 14:40:23 +0300

    Added Document to JsObject and Elastic index Flows

--- src/main/scala/stream/MongoToElasticsearch.scala
+++ src/main/scala/stream/MongoToElasticsearch.scala
@@ -1,27 +1,43 @@
 package stream
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model.HttpRequest
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Flow, Source}
 import mongo.MongoCollectionFactory
 import org.mongodb.scala.{Document, FindObservable}
+import play.api.libs.json.{JsObject, Json}
 import rxStreams.Implicits._
 
-object MongoToElasticsearch {
+object MongoToElasticsearch extends App {
 
-  def main(args: Array[String]): Unit = {
-    implicit val system = ActorSystem("Sys")
-    import system.dispatcher
+  implicit val system = ActorSystem("Sys")
 
-    implicit val materializer = ActorMaterializer()
+  import system.dispatcher
 
-    val coll = new MongoCollectionFactory().makeCollection("products")
+  implicit val materializer = ActorMaterializer()
 
-    val find: FindObservable[Document] = coll.find()
+  val coll = new MongoCollectionFactory().makeCollection("galleries")
 
-    Source.fromPublisher(find)
-      .runForeach(d => println(s"Indexing Document: \n $d"))
-      .onComplete(_ => println("Indexing complete"))
+  val find: FindObservable[Document] = coll.find()
 
+  def index = Flow[JsObject].map { doc =>
+    println(s"Indexing $doc")
+    Http().singleRequest(HttpRequest(POST, "http://localhost:9200/rightnow/faq", entity = doc.toString()))
   }
+
+  def toJson = Flow[Document].map{ doc=>
+    Json.parse(doc.toJson()).as[JsObject] - "_id"
+  }
+
+  Source.fromPublisher(find)
+    .via(toJson)
+    .via(index)
+    .runForeach(d => println(s"Indexed Document: \n $d"))
+    .onComplete(_ => println("Indexing complete"))
+
 }
+
+
